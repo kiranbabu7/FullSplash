fdb67f6cccbaaedacad4beea0c3891a5e32e0824
Kiran Babu <kiranbabu7675@gmail.com>
2022-09-21 21:41:04 +0530

    feat: complete Image upload card UI

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ImageUploaderComponent } from './image-uploader/image-uploader.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImageUploaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

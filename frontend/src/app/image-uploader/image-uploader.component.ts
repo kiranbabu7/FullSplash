3ce3c5c3e9f671d676a3a60fbe60e35caea83bee
Kiran Babu <kiranbabu7675@gmail.com>
2022-09-22 23:50:43 +0530

    feat: add drag and drop file feature

--- frontend/src/app/image-uploader/image-uploader.component.ts
+++ frontend/src/app/image-uploader/image-uploader.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageUploaderComponent implements OnInit {
 
+  file!: File;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  uploadFile(event: any) {
+    this.file = event.target.files[0]
+    console.log(this.file)
+  }
 }

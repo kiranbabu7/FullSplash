9eb0c2be9542b9efb647ace1a8a95747d5c64b7a
Kiran Babu <kiranbabu7675@gmail.com>
2022-09-25 00:16:33 +0530

    refactor: remove django admin page

--- backend/full_splash/full_splash/urls.py
+++ backend/full_splash/full_splash/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('upload-image/', include('image_uploader.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
